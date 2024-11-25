744574448c9fb3f92b80668b05eb9836e9bed2c7
Aleksandr Kerov <kerov95@gmail.com>
2024-11-25 23:50:43 +0000

    refactor methods; enhancements

--- 2_lesson/run.py
+++ 2_lesson/run.py
@@ -1,58 +1,91 @@
-from typing import Union
+import textwrap
 
 COMMANDS = ("quit", "show", "retrieve", "add")
 
 # Simulated database
 students = [
     {
+        "id": 0,
         "name": "John Doe",
         "marks": [4, 5, 1, 4, 5, 2, 5],
-        "info": "John is 22 y.o. Hobbies: music",
+        "info": "John is 22 y.o. Hobbies: music"
     },
     {
+        "id": 1,
         "name": "Marry Black",
         "marks": [4, 1, 3, 4, 5, 1, 2, 2],
-        "info": "John is 23 y.o. Hobbies: football",
-    },
+        "info": "John is 23 y.o. Hobbies: football"
+    }
 ]
 
 
-def find_student(name: str) -> Union[dict, None]:
-    for student in students:
-        if student["name"] == name:
-            return student
-
-    return None
+def find_student(*, value: str, key: str = "name") -> dict | None:
+    return next((student for student in students if student.get(key) == value), None)
 
 
 def show_students() -> None:
     print("=" * 20)
+
     print("The list of students:\n")
-    for student in students:
-        print(f"{student['name']}. Marks: {student['marks']}")
+    print(
+        *[f"{student.get('name')}. Marks:{student['marks']
+                                          }" for student in students],
+        sep="\n"
+    )
 
     print("=" * 20)
 
 
-def show_student(name: str) -> None:
-    student: dict | None = find_student(name)
+def show_student(*, value: str, key: str = "name") -> None:
+    student: dict | None = find_student(value=value, key=key)
+    if student:
+        print_student_info(student)
+    else:
+        print(f"There is no student with {key} = {value}")
 
-    if not student:
-        print(f"There is no student {name}")
-        return
 
+def print_student_info(student: dict) -> None:
     print("Detailed about student:\n")
     print(
         f"{student['name']}. Marks: {student['marks']}\n"
-        f"Details: {student['info']}\n"
+        f"Details: {textwrap.fill(student['info'], width=50)}\n"
     )
 
 
-def add_student(student_name: str):
-    instance = {"name": student_name, "marks": [], "info": None}
+def add_student(student_name: str, details: str | None):
+    instance = {"name": student_name, "marks": [], "info": details}
     students.append(instance)
 
     return instance
+
+
+def retrieve_handle():
+    allowed_keys = ("id", "name")
+    key = input(
+        f"Would you like to search for a student by {
+            ', '.join(allowed_keys)}?\n"
+    )
+
+    if key not in allowed_keys:
+        print(f"A student doesn't have a {key} property.\n")
+        return
+
+    value = input(f"Enter student {key} you are looking for: ")
+
+    if key == "id":
+        try:
+            value = int(value)
+        except ValueError:
+            raise ValueError("Invalid input. ID should be a number.")
+
+    show_student(value=value, key=key)
+
+
+def add_handle():
+    add_student(
+        input("Enter student's name: "),
+        input("Enter student's details: ")
+    )
 
 
 def main():
@@ -72,11 +105,9 @@ def main():
             if user_input == "show":
                 show_students()
             elif user_input == "retrieve":
-                student_name = input("Enter student name you are looking for: ")
-                show_student(student_name)
+                retrieve_handle()
             elif user_input == "add":
-                name = input("Enter student's name: ")
-                add_student(name)
+                add_handle()
         except NotImplementedError as error:
             print(f"Feature '{error}' is not ready for live.")
         except Exception as error:
