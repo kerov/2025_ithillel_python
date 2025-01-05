03b1336bd3795065cac3dc50ce7172c7f20fa6d5
Aleksandr Kerov <kerov95@gmail.com>
2025-01-05 20:14:22 +0000

    lesson-9; ABC SocialChannel logic

--- 9_lesson/run.py
+++ 9_lesson/run.py
@@ -1,24 +1,59 @@
-# each social channel has a type
-# and the current number of followers
-SocialChannel = tuple[str, int]
-
-# each post has a message and the timestamp when it should be posted
-Post = tuple[str, int]
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from time import time
 
 
-def post_a_message(channel: SocialChannel, message: str) -> None:
-    type, _ = channel
-    if type == "youtube":
-        post_to_youtube(channel, message)
-    elif type == "facebook":
-        post_to_facebook(channel, message)
-    elif type == "twitter":
-        post_to_twitter(channel, message)
+class SocialChannel(ABC):
+    def __init__(self, followers: int):
+        self.followers = followers
+
+    @abstractmethod
+    def post_a_message(self, message: str):
+        pass
+
+
+class YouTubeChannel(SocialChannel):
+    def post_a_message(self, message: str):
+        print(f"YouTube: {message}")
+
+
+class FacebookChannel(SocialChannel):
+    def post_a_message(self, message: str):
+        print(f"Facebook: {message}")
+
+
+class TwitterChannel(SocialChannel):
+    def post_a_message(self, message: str):
+        print(f"Twitter: {message}")
+
+
+@dataclass
+class Post:
+    message: str
+    timestamp: int
+
+    def is_time_to_publish(self, publish_time: int):
+        return self.timestamp <= publish_time
 
 
 def process_schedule(posts: list[Post], channels: list[SocialChannel]) -> None:
+    current_time = time()
     for post in posts:
-        message, timestamp = post
-        for channel in channels:
-            if timestamp <= time():
-                post_a_message(channel, message)
+        if post.is_time_to_publish(current_time):
+            for channel in channels:
+                channel.post_a_message(post.message)
+
+
+process_schedule(
+    [
+        Post("Past post 1", timestamp=int(time()) - 10),
+        Post("Current post", timestamp=int(time())),
+        Post("Not yet.", timestamp=int(time()) + 60),
+        Post("Past post 2", timestamp=int(time()) - 20)
+    ],
+    [
+        YouTubeChannel(followers=100),
+        FacebookChannel(followers=50),
+        TwitterChannel(followers=222)
+    ]
+)
